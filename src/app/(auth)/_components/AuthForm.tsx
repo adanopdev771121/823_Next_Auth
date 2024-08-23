74217b24bf62521664bf2312beb61fe3800379c3
Austen <austinanop@gmail.com>
2024-08-23 14:45:24 -0400

    Implement submit validation

--- src/app/(auth)/_components/AuthForm.tsx
+++ src/app/(auth)/_components/AuthForm.tsx
@@ -22,22 +22,38 @@ export default function AuthForm({ title }: { title: string }) {
   const [isPasswordShown, setIsPasswordShown] = useState<boolean>(false);
 
   return (
-    <form onSubmit={onSubmit} className="mt-14 relative">
+    <form onSubmit={onSubmit} className="mt-14 h-[380px] relative">
       <label className="font-bold text-xl">Email</label>
       <input
-        {...register("email")}
-        className="w-full h-20 rounded-xl border border-solid border-gray-300 text-xl px-5 mb-6"
+        {...register("email", {
+          required: { value: true, message: "Email field is required" },
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "Invalid Email",
+          },
+        })}
+        className="w-full h-20 rounded-xl border border-solid border-gray-300 text-xl px-5"
       />
+      {errors.email && <p className="text-red-500">{errors.email.message}</p>}
       <div className="absolute top-[60px] right-5">
         <IconMail />
       </div>
-      <label className="font-bold text-xl">Password</label>
+      <p className="font-bold text-xl mt-6">Password</p>
       <input
-        {...register("password")}
+        {...register("password", {
+          required: { value: true, message: "Password field is required" },
+          minLength: {
+            value: 6,
+            message: "Password must be more than 6 characters",
+          },
+        })}
         type={isPasswordShown ? "text" : "password"}
         placeholder="Enter Password"
         className="w-full h-20 rounded-xl border border-solid border-gray-300 text-xl px-5"
       />
+      {errors.password && (
+        <p className="text-red-500">{errors.password.message}</p>
+      )}
       <div
         className="absolute top-[190px] right-5"
         onClick={() => setIsPasswordShown(!isPasswordShown)}
@@ -47,7 +63,7 @@ export default function AuthForm({ title }: { title: string }) {
       <input
         type="submit"
         value={title}
-        className="w-full h-20 bg-green-500 font-bold text-white rounded-xl text-xl text-center mt-12"
+        className="w-full h-20 bg-green-500 font-bold text-white rounded-xl text-xl text-center absolute bottom-1 left-0"
       />
     </form>
   );
